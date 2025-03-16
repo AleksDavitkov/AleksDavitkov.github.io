eb4d01f74e289723994fc14f561373a07fb39670
AleksDavitkov <aleksdavitkov@hotmail.com>
2025-03-16 20:11:13 +0100

    classes push

---
+++ homework/c#_basic/homework5_c#_classes/classes.RaceCars.App/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace classes.RaceCars.App
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Driver Driver { get; set; }
+
+        public Car(string model, int speed, Driver driver)
+        {
+            Model = model;
+            Speed = speed;
+            Driver = driver;
+        }
+
+        public int CalculateSpeed(Driver driver)
+        {
+            return driver.Skill * Speed;
+        }
+
+        public static void RaceCars(Car raceCar1, Car raceCar2)
+        {
+            int car1Speed = raceCar1.CalculateSpeed(raceCar1.Driver);
+            int car2Speed = raceCar2.CalculateSpeed(raceCar2.Driver);
+
+            if (car1Speed > car2Speed)
+            {
+                Console.WriteLine($"{raceCar1.Driver.Name} with the {raceCar1.Model} won! Speed: {raceCar1.Speed} km/h, Driver's Skill: {raceCar1.Driver.Skill}");
+            }
+            else if (car2Speed > car1Speed)
+            {
+                Console.WriteLine($"{raceCar2.Driver.Name} with the {raceCar2.Model} won! Speed: {raceCar2.Speed} km/h, Driver's Skill: {raceCar2.Driver.Skill}");
+            }
+            else
+            {
+                Console.WriteLine("It's a tie! Maybe they should beer pong to settle their differences.");
+            }
+        }
+    }
+}
---
+++ homework/c#_basic/homework5_c#_classes/classes.RaceCars.App/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace classes.RaceCars.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver[] drivers = Drivers();
+            Car[] cars = Cars(drivers);
+
+            Car.RaceCars(cars[0], cars[2]);
+            Car.RaceCars(cars[1], cars[3]);
+
+            // OR Let the user select cars for the race
+            RaceAgain(cars);
+        }
+
+        static Driver[] Drivers()
+        {
+            return new Driver[]
+            {
+                new Driver("Bob", 66),
+                new Driver("Greg", 83),
+                new Driver("Jill", 68),
+                new Driver("Ane", 83)
+            };
+        }
+
+        // Cars assigned to designated drivers
+        static Car[] Cars(Driver[] drivers)
+        {
+            return new Car[]
+            {
+                new Car("Ferrari", 240, drivers[0]),
+                new Car("Porsche", 220, drivers[1]),
+                new Car("Mercedes", 230, drivers[2]),
+                new Car("Opel Astra", 220, drivers[3])
+            };
+        }
+
+        static Car[] SelectCars(Car[] cars)
+        {
+            // I could go on here with additional input validation and error handling though I think it's good enough for now
+
+            Console.WriteLine("\nChoose your first car for the race:");
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {cars[i].Model}");
+            }
+            int firstCarChoice = int.Parse(Console.ReadLine()) - 1; // Here we subtract 1 from the user's input to match the array index (user friendly)
+
+            Console.WriteLine("\nChoose your second car for the race:");
+            for (int i = 0; i < cars.Length; i++)
+            {
+                if (i != firstCarChoice)
+                {
+                    Console.WriteLine($"{i + 1}. {cars[i].Model}");
+                }
+            }
+            int secondCarChoice = int.Parse(Console.ReadLine()) - 1;
+
+            return new Car[] { cars[firstCarChoice], cars[secondCarChoice] };
+        }
+
+        static void RaceAgain(Car[] cars)
+        {
+            bool raceAgain = true;
+            while (raceAgain)
+            {
+                // Pulls from the SelectCars method
+                Car[] selectedCars = SelectCars(cars);
+                Car.RaceCars(selectedCars[0], selectedCars[1]);
+
+                Console.WriteLine("\nDo you want to race again? (yes/no)");
+                string userResponse = Console.ReadLine().ToLower();
+
+                if (userResponse != "yes")
+                {
+                    raceAgain = false; // End loop if the user doesn't want to race again
+                }
+            }
+
+            Console.WriteLine("Thank you for racing!");
+        }
+    }
+}
